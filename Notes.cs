f6f78753d950b0bde08839caa4e710dc10afcc01
Carlos <cvirgili438@gmail.com>
2023-05-02 13:42:57 -0300

    Agregar archivos de proyecto.

---
+++ Notes.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesApp
+{
+    internal class Notes
+    {
+        private DataTable _dt = new DataTable();
+        private string _name; 
+        public Notes(string configString, string name)
+        {
+            using (SqlConnection sql = new SqlConnection(configString))
+            {
+                using (SqlCommand cmd = new SqlCommand("sp_get_user_notes", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    sql.Open();
+                    cmd.Parameters.Add(new SqlParameter("@Nombre", $"%{name}%"));
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            Console.WriteLine($"Para el sr {reader.GetString(reader.GetOrdinal("Nombre"))}");
+                            while (reader.Read())
+                            {
+                                
+                                Console.WriteLine($"tiene la tarea de {reader.GetString(reader.GetOrdinal("texto"))} para el dia {reader.GetDateTime(reader.GetOrdinal("to_day")).ToString("dd/MM/yyyy")}");
+                            }
+                            var dt = new DataTable();
+                            var da = new SqlDataAdapter(cmd);
+                            da.Fill(dt);
+                            _dt= dt;
+                        }
+                        else
+                        {
+                            Console.WriteLine("No se encontraron notas para el usuario especificado.");
+                            Program
+                        }
+                    }
+                }
+            }
+
+            Console.Write("You wanna create new Task ?");
+            string response = Console.ReadLine();
+            if (response == "y" || response == "Y") 
+            { 
+                CreateNote newNote = new CreateNote();
+            }
+            
+        }
+        public DataTable PrintDataTable { 
+            get 
+            {
+                
+                return _dt;
+            } 
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace NotesApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to your notes applications");
+            Console.WriteLine("Please, Type your name ");    
+            string name = Console.ReadLine(); 
+            var configurationString = ConfigurationManager.ConnectionStrings["defaultConnection"].ConnectionString;
+
+            Notes getNote = new Notes(configurationString,name);
+            
+            
+               
+        }
+        public static void RestartApplication()
+        {
+
+            Console.Write("Desea reiniciar el programa ? Y/N ");
+            string response = Console.ReadLine();
+            if (response.ToUpper() == "N")
+            {
+                Environment.Exit(0);
+            }
+            else if (response.ToUpper() == "Y")
+            {
+                Main(new string[] { });
+            }
+            else { RestartApplication(); }
+        }
+    }
+}
